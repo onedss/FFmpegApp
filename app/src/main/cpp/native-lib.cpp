63d8b5d1c0be6f66d67f6852062b303c54d3423a
onedss <onedss@qq.com>
2023-01-26 12:44:45 +0800

    add open file function.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,8 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, "FFmpegApp", __VA_ARGS__);
 extern "C"{
 #include <libavcodec/avcodec.h>
 }
@@ -12,13 +15,36 @@ Java_com_vpaypay_ffmpegapp_MainActivity_stringFromJNI(
     hello += avcodec_configuration();
     return env->NewStringUTF(hello.c_str());
 }
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_vpaypay_ffmpegapp_MainActivity_start(JNIEnv *env, jobject thiz) {
     // TODO: implement start()
 }
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_vpaypay_ffmpegapp_MainActivity_stop(JNIEnv *env, jobject thiz) {
     // TODO: implement stop()
+}
+
+
+extern "C"
+JNIEXPORT jboolean JNICALL
+Java_com_vpaypay_ffmpegapp_MainActivity_open(JNIEnv *env, jobject thiz, jstring j_url,
+                                             jobject handler) {
+
+    const char *url = env->GetStringUTFChars(j_url, 0);
+    FILE *fp = fopen(url, "rb");
+    if(!fp)
+    {
+        LOGW("File %s open failed!", url);
+    }
+    else
+    {
+        LOGW("File %s open succeed!", url);
+        fclose(fp);
+    }
+    env->ReleaseStringUTFChars(j_url, url);
+    return false;
 }
--- app/src/main/java/com/vpaypay/ffmpegapp/MainActivity.java
+++ app/src/main/java/com/vpaypay/ffmpegapp/MainActivity.java
@@ -3,11 +3,14 @@ package com.vpaypay.ffmpegapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vpaypay.ffmpegapp.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     // Used to load the 'ffmpegapp' library on application startup.
     static {
@@ -23,9 +26,20 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Button btn1 =findViewById(R.id.btn_start);
+        Button btn2 =findViewById(R.id.btn_stop);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+
+        //open("/sdcard/1080.mpr", this);
+
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
+
+        //for(int i=0;i<1000;i++) {
+            open("/sdcard/in1.mp4", this);
+        //}
     }
 
     /**
@@ -34,7 +48,25 @@ public class MainActivity extends AppCompatActivity {
      */
     public native String stringFromJNI();
 
+    public native boolean open(String url, Object handler);
+
     public native void start();
 
     public native void stop();
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_start:
+                //btn1按钮点击触发的事件
+                Toast.makeText(this, "开始推流点击触发的事件", Toast.LENGTH_SHORT).show();
+                start();
+                break;
+            case R.id.btn_stop:
+                //btn2按钮点击触发的事件
+                Toast.makeText(this, "停止推流点击触发的事件", Toast.LENGTH_SHORT).show();
+                stop();
+                break;
+        }
+    }
 }
